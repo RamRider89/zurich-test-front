b3a04ffda85e413d704e1c279ad0330979e7e731
David D <david.duarte@davdav.tech>
2025-01-18 17:50:14 -0700

    init componentes

---
+++ src/app/poliza-form/poliza-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poliza-form',
+  imports: [],
+  templateUrl: './poliza-form.component.html',
+  styleUrl: './poliza-form.component.css'
+})
+export class PolizaFormComponent {
+
+}
---
+++ src/app/poliza-list/poliza-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poliza-list',
+  imports: [],
+  templateUrl: './poliza-list.component.html',
+  styleUrl: './poliza-list.component.css'
+})
+export class PolizaListComponent {
+
+}
